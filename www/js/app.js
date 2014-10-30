f067fbe3d593e898157b32f2b8a34b7654c83649
Rony Moura <rony-dev@hotmail.com>
2014-10-30 10:11:51 -0200

    Página principal e navegação
    Criação da página principal e da navegação do menu principal

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+/*var app = angular.module('wppmc', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,65 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+});
+
+function MyCtrl($scope, $ionicNavBarDelegate) {
+  $scope.goBack = function() {
+    $ionicNavBarDelegate.back();
+  };
+}*/
+
+var app = angular.module('wppmc', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('index', {
+        url: '/',
+        templateUrl: 'index.html'
+    })
+    .state('audio', {
+        url: '/audio',
+        templateUrl: 'audio.html'
+    })
+    .state('imagem', {
+        url: '/imagem',
+        templateUrl: 'imagem.html'
+    })
+    .state('video', {
+        url: '/video',
+        templateUrl: 'video.html'
+    })
+    .state('galeria', {
+        url: '/galeria',
+        templateUrl: 'galeria.html'
+    })
+
+    $urlRouterProvider.otherwise("/");
 })
+
+.factory('MenuItemsFactory', function () {
+
+    var menuItems = [];
+
+    $.getJSON("js/data.json", {}, function(data) {
+        for(i = 0; i < data.items.length; i++) {
+          var menuItem = data.items[i];
+
+          menuItems.push(menuItem);
+        }
+    });
+
+    return {
+        all: function () {
+            return menuItems;
+        },
+
+        get: function (id) {
+            return menuItems[id];
+        }
+    };
+})
+
+.controller('MainController', function($scope, $stateParams, MenuItemsFactory) {
+    $scope.menuItems = MenuItemsFactory.all();
+});
